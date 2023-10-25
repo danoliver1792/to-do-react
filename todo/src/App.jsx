2d5604be34d71544cedc6e658ddf8bc0d5f3823c
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-10-25 20:37:48 -0300

    segunda parte do to-do com React

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -24,7 +24,28 @@ function App() {
     }
   ]);
 
-  return <div>Ola, React</div>
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {/* percorrendo todo array e exibindo cada item do to-do com map */}
+      {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">
+                ({todo.category})
+              </p>
+            </div>
+
+            {/* div para separar os botoes */}
+            <div>
+              <button>Completar</button>
+              <button>Deletar</button>
+            </div>
+          </div>
+        ))}
+    </div>
+  </div>
 }
 
 export default App
